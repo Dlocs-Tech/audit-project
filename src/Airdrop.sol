dac14e5ca1bbc72836203e4862c9d83bd5b847a4
wafflemakr <wafflemakr@gmail.com>
2023-10-19 10:18:32 -0500

    fix stateless fuzzy airdrop tests

--- src/Airdrop.sol
+++ src/Airdrop.sol
@@ -4,15 +4,35 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/token/ERC20/IERC20.sol";
 
 contract Airdrop {
-    function airdropToken(address _token, address[] calldata _receivers, uint256[] calldata _amounts) external {
-        require(_receivers.length == _amounts.length, "_receivers.length not the same as _amounts.length");
+    function airdropToken(
+        address _token,
+        address[] calldata _receivers,
+        uint256[] calldata _amounts
+    ) external {
+        require(
+            _receivers.length == _amounts.length,
+            "_receivers.length not the same as _amounts.length"
+        );
         for (uint256 i; i < _receivers.length; i++) {
-            require(IERC20(_token).transferFrom(tx.origin, _receivers[i], _amounts[i]), "Token send failed");
+            require(
+                IERC20(_token).transferFrom(
+                    tx.origin,
+                    _receivers[i],
+                    _amounts[i]
+                ),
+                "Token send failed"
+            );
         }
     }
 
-    function airdropMatic(address payable[] calldata _receivers, uint256[] calldata _amounts) external payable {
-        require(_receivers.length == _amounts.length, "_receivers.length not the same as _amounts.length");
+    function airdropMatic(
+        address payable[] calldata _receivers,
+        uint256[] calldata _amounts
+    ) external payable {
+        require(
+            _receivers.length == _amounts.length,
+            "_receivers.length not the same as _amounts.length"
+        );
         for (uint256 i; i < _receivers.length; i++) {
             _receivers[i].transfer(_amounts[i]);
         }
--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.18;
 import "@openzeppelin/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor() ERC20 ("Mock ERC20", "MERC") {
+    constructor() ERC20("Mock ERC20", "MERC") {
         _mint(msg.sender, 1000000000e18);
     }
 }
